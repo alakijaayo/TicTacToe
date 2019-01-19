178de8e390c4fb1c8819c1e561ce1ca458217777
alakijaayo <alakijaayo@gmail.com>
2019-01-19 17:35:42 +0000

    Adding winner method
    Have added a method to determine who is the winner. Trying to figure out how best to test this option. With me having to create a loop to get a winner, trying to see if there is a way to mock player 1 or 2 and the array to test it out

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,36 @@
 class Game
 
-def player_1_move
-  puts "Player 1, choose your position:"
-  answer = gets.chomp.upcase
-end
+  WIN_OPTIONS = [
+    ["BC", "BL", "BR"],
+    ["BC", "MC", "TC"],
+    ["BL", "MC", "TR"],
+    ["BL", "ML", "TL"],
+    ["BR", "MC", "TL"],
+    ["BR", "MR", "TR"],
+    ["MC", "ML", "MR"],
+    ["TC", "TL", "TR"]
+  ]
 
-def player_2_move
-  puts "Player 2, choose your position:"
-  answer2 = gets.chomp.upcase
-end
+  def initialize
+    @player1 = []
+    @player2 = []
+  end
+
+  def player_1_move
+    puts "Player 1, choose your position:"
+    answer = gets.chomp.upcase
+    @player1.push(answer)
+  end
+
+  def player_2_move
+    puts "Player 2, choose your position:"
+    answer2 = gets.chomp.upcase
+    @player2.push(answer2)
+  end
+
+  def winner
+    WIN_OPTIONS.include?(@player1[0..2].sort) ? "Player 1 Wins" : WIN_OPTIONS.include?(@player2[0..2].sort) ? "Player 2 Wins" : "It's a Draw"
+  end
+
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,5 @@
 require './lib/game'
+ 
 
 describe Game do
   it "asks player one to pick a move" do
@@ -6,9 +7,9 @@ describe Game do
     expect { subject.player_1_move }.to output("Player 1, choose your position:\n").to_stdout
   end
 
-  it "should receive an answer from player 1" do
+  it "should receive and store answer from player 1" do
     allow_any_instance_of(Game).to receive(:gets).and_return("TL")
-    expect(subject.player_1_move).to eq("TL")
+    expect(subject.player_1_move).to eq(["TL"])
   end
 
   it "asks player two to pick a move" do
@@ -16,9 +17,9 @@ describe Game do
     expect { subject.player_2_move }.to output("Player 2, choose your position:\n").to_stdout
   end
 
-  it "should receive an answer from player 2" do
+  it "should receive and store answer from player 2" do
     allow_any_instance_of(Game).to receive(:gets).and_return("TR")
-    expect(subject.player_2_move).to eq("TR")
+    expect(subject.player_2_move).to eq(["TR"])
   end
 
 end
