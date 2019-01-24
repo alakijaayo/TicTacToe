b20ed5c61ffd1416943d3876daeb8c29c1e79e04
alakijaayo <alakijaayo@gmail.com>
2019-01-24 10:10:01 +0000

    Updating Board Class
    Adding a way for the board to collect and store the position of X and O on the board before printing. Went through a few different options, and original was a bit longer, but thanks to some help I was able to get to the one I have now. Have tested some of it but not been able to test the elsif statement in user choice

--- lib/board.rb
+++ lib/board.rb
@@ -1,34 +1,46 @@
 class Board
 
-  def initialize(output: $stdout)
+  attr_accessor :choices
+
+  def initialize(output: $stdout, input: $stdin)
+    @input = input
     @output = output
-  end
-
-  def squares
-    [@TL, @TC, @TR, @ML, @MC, @MR, @BL, @BC, @BR]
-  end
-
-  def new_board
-    @TL = " "
-    @TC = " "
-    @TR = " "
-    @ML = " "
-    @MC = " "
-    @MR = " "
-    @BL = " "
-    @BC = " "
-    @BR = " "
+    @choices = {
+      "TL" => " ",
+      "TC" => " ",
+      "TR" => " ",
+      "ML" => " ",
+      "MC" => " ",
+      "MR" => " ",
+      "BL" => " ",
+      "BC" => " ",
+      "BR" => " "
+    }
   end
 
   def print_board
     @output.puts
     @output.puts "   L   C   R"
-    @output.puts "T  #{@TL} | #{@TC} | #{@TR} "
+    @output.puts "T  #{@choices["TL"]} | #{@choices["TC"]} | #{@choices["TR"]} "
     @output.puts "  ---|---|---"
-    @output.puts "M  #{@ML} | #{@MC} | #{@MR} "
+    @output.puts "M  #{@choices["ML"]} | #{@choices["MC"]} | #{@choices["MR"]} "
     @output.puts "  ---|---|---"
-    @output.puts "B  #{@BL} | #{@BC} | #{@BR} "
+    @output.puts "B  #{@choices["BL"]} | #{@choices["BC"]} | #{@choices["BR"]} "
     @output.puts
+  end
+
+  def user_choice(answer, user)
+    if available?(answer)
+      @choices[answer] = user
+    elsif available?(answer) == false
+      puts "This position is already taken. Try again:"
+      answer = @input.gets.chomp.upcase
+      user_choice(answer, user)
+    end
+  end
+
+  def available?(position)
+    @choices[position] == " "
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,6 +7,22 @@ describe Board do
     board = Board.new(output: output)
     board.print_board
     output.rewind
-    expect(output.read).to eq("\n   L   C   R\nT  #{@TL} | #{@TC} | #{@TR} \n  ---|---|---\nM  #{@ML} | #{@MC} | #{@MR} \n  ---|---|---\nB  #{@BL} | #{@BC} | #{@BR} \n\n")
+    expect(output.read).to eq("\n   L   C   R\nT  #{board.choices["TL"]} | #{board.choices["TC"]} | #{board.choices["TR"]} \n  ---|---|---\nM  #{board.choices["ML"]} | #{board.choices["MC"]} | #{board.choices["MR"]} \n  ---|---|---\nB  #{board.choices["BL"]} | #{board.choices["BC"]} | #{board.choices["BR"]} \n\n")
   end
+
+  it "takes user choice and changes position" do
+    subject.user_choice("TL", "X")
+    expect(subject.choices["TL"]).to eq("X")
+  end
+
+  it "checks if a position is available" do
+    expect(subject.available?("TL")).to eq(true)
+  end
+
+  it "informs you if a position is not available" do
+    subject.choices["TL"] = "X"
+    expect(subject.available?("TL")).to eq(false)
+
+  end
+
 end
