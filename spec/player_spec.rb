b6833be278c441293d6bfa4d7f23039af8e94d63
alakijaayo <alakijaayo@gmail.com>
2019-02-26 10:16:12 +0000

    Adding a few tests

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,6 +2,14 @@ require './lib/player'
 
 describe Player do
 
+  it "shows the move player one has made" do
+    expect(subject.move_x("TC")).to eq("TC")
+  end
+
+  it "shows the move player two has made" do
+    expect(subject.move_o("TL")).to eq("TL")
+  end
+
   it "informs Player 1 wins" do
     subject.move_x("TL")
     subject.move_x("TR")
